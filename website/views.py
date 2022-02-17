e8c024625fc330c0bc1d168254e9566b83195755
majeedkarimi <46189036+majeedkarimi@users.noreply.github.com>
2022-02-17 10:22:10 +0330

    set general setting
    base cv

--- website/views.py
+++ website/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 
 def index_view(request):
-    return render(request, "index.html")
+    content={"name":"Majid","lastname":"Karimi"}
+    return render(request, "index.html", content)
